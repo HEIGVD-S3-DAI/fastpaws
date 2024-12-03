05188130df09732edf842639a9593fde312824d1
Aude Laydu <audel1814@gmail.com>
2024-12-03 17:55:47 +0100

    Implemented new logic for choosing game text (need to modify with real interesting texts)

--- src/main/java/ch/heigvd/dai/logic/server/TypingGame.java
+++ src/main/java/ch/heigvd/dai/logic/server/TypingGame.java
@@ -4,24 +4,11 @@ import java.util.Random;
 
 public class TypingGame {
   public static final int MIN_PLAYERS_FOR_GAME = 2;
-  private static final int NUM_WORDS = 20;
+  private static String[] PARAGRAPHS = {"Text 1 ", "Text 2", "Text 3", "Text 4", "Text 5"};
 
   public static String getParagraph() {
     Random random = new Random();
-    StringBuilder paragraph = new StringBuilder();
-
-    for (int i = 0; i < TypingGame.NUM_WORDS; i++) {
-      int wordLength = 2 + random.nextInt(5); // Random word length between 2 and 6
-      StringBuilder word = new StringBuilder();
-
-      for (int j = 0; j < wordLength; j++) {
-        char letter = (char) ('a' + random.nextInt(26)); // Generate a random letter
-        word.append(letter);
-      }
-
-      paragraph.append(word).append(" ");
-    }
-
-    return paragraph.toString().trim();
+    int indexChosen = random.nextInt(PARAGRAPHS.length);
+    return PARAGRAPHS[indexChosen];
   }
 }
